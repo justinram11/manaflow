52bdf758b7f52913363a2fba668f29c9222e3744
austinpower1258 <austinwang115@gmail.com>
2025-12-09 18:56:03 -0800

    ok

--- packages/www-openapi-client/src/client/types.gen.ts
+++ packages/www-openapi-client/src/client/types.gen.ts
@@ -420,7 +420,7 @@ export type SetupInstanceBody = {
     instanceId?: string;
     selectedRepos?: Array<string>;
     ttlSeconds?: number;
-    snapshotId?: string | ('snapshot_d214mmqt' | 'snapshot_8ypfo3lq' | 'snapshot_pcmfvjra');
+    snapshotId?: string | ('snapshot_vkfzdrxw' | 'snapshot_jqe2lrhp' | 'snapshot_pcmfvjra');
 };
 
 export type CreateEnvironmentResponse = {
