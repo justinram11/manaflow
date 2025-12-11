c71a1fca330ff1350aa36a4c3c5d7fea2687f9b0
Lawrence Chen <54008264+lawrencecchen@users.noreply.github.com>
2025-12-10 20:58:45 -0800

    wip

--- packages/www-openapi-client/src/client/types.gen.ts
+++ packages/www-openapi-client/src/client/types.gen.ts
@@ -420,7 +420,7 @@ export type SetupInstanceBody = {
     instanceId?: string;
     selectedRepos?: Array<string>;
     ttlSeconds?: number;
-    snapshotId?: string | ('snapshot_zqwdsowy' | 'snapshot_s7alqj3n' | 'snapshot_pcmfvjra');
+    snapshotId?: string | ('snapshot_9notogts' | 'snapshot_wvxt73bo' | 'snapshot_pcmfvjra');
 };
 
 export type CreateEnvironmentResponse = {
